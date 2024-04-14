3ad4cab70d73bde7eb0a483e7656dbfc44025e05
jnmorais <jsmorais2002@gmail.com>
2024-04-14 02:29:24 -0300

    primeiro commit

---
+++ src/main/java/org/academify/controller/AlunoController.java
@@ -0,0 +1,52 @@
+package org.academify.controller;
+
+import org.academify.model.entity.Aluno;
+import org.academify.model.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/aluno")
+public class AlunoController {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @GetMapping("/listar")
+    public List<Aluno> listar() {
+        return alunoRepository.findAll();
+    }
+
+    @GetMapping("/getById/{id}")
+    public Optional<Aluno> getById(@PathVariable("id") int id) {
+        return alunoRepository.findById(id);
+    }
+
+    @GetMapping("/total")
+    public Long getTotal() {
+        return alunoRepository.count();
+    }
+
+    @PostMapping("/create")
+    public Aluno create(@RequestBody Aluno aluno) {
+        return alunoRepository.save(aluno);
+    }
+
+    @PutMapping("/edit")
+    public Aluno edit(@RequestBody Aluno aluno) {
+        return alunoRepository.save(aluno);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Aluno delete(@PathVariable("id") int id) {
+        Optional<Aluno> alunoQueEuQueroRemover = alunoRepository.findById(id);
+        if (!alunoQueEuQueroRemover.isPresent()) return null;
+
+        alunoRepository.delete(alunoQueEuQueroRemover.get());
+        return alunoQueEuQueroRemover.get();
+    }
+
+}
---
+++ src/test/java/org/academify/AcademifyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.academify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
